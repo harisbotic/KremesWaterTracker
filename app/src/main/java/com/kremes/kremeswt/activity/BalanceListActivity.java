216bafeea0cc3ef013ea6cad9ccb583a35dd6cf2
harisbotic <haris.botic96@gmail.com>
2017-11-12 15:56:21 +0100

    Sort Balances

--- app/src/main/java/com/kremes/kremeswt/activity/BalanceListActivity.java
+++ app/src/main/java/com/kremes/kremeswt/activity/BalanceListActivity.java
@@ -8,8 +8,10 @@ import android.widget.LinearLayout;
 import com.kremes.kremeswt.R;
 import com.kremes.kremeswt.database.KremesDatabase;
 import com.kremes.kremeswt.entity.Citizen;
+import com.kremes.kremeswt.model.CitizenWithReport;
 import com.kremes.kremeswt.views.BalanceCard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BalanceListActivity extends AppCompatActivity {
@@ -35,11 +37,25 @@ public class BalanceListActivity extends AppCompatActivity {
             }
 
             protected void onPostExecute(List<Citizen> allCitizens) {
-                for (Citizen citizen: allCitizens) {
+                List<Citizen> newCitizenList = sortCitizensByBalance(allCitizens);
+                for (Citizen citizen: newCitizenList) {
                     balanceCardHolder.addView(new BalanceCard(BalanceListActivity.this, citizen));
                 }
             }
         }.execute();
+    }
+
+    private List<Citizen> sortCitizensByBalance(List<Citizen> citizenList) {
+        ArrayList<Citizen> positiveBalances = new ArrayList<>();
+        ArrayList<Citizen> negativeBalances = new ArrayList<>();
+        for (Citizen citizen: citizenList) {
+            if(citizen.getBalance() >= 0)
+                positiveBalances.add(citizen);
+            else
+                negativeBalances.add(citizen);
+        }
+        negativeBalances.addAll(positiveBalances);
+        return negativeBalances;
     }
 
     @Override
