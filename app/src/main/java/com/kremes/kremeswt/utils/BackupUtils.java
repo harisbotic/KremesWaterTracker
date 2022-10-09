7ba62f87948c719e8dba4ae17649d2896a5fcc91
Haris Botic <hbotic@vaulthealth.com>
2022-10-09 18:56:16 +0200

    implement share backup

--- app/src/main/java/com/kremes/kremeswt/utils/BackupUtils.java
+++ app/src/main/java/com/kremes/kremeswt/utils/BackupUtils.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.widget.Toast;
 
 import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
+import com.kremes.kremeswt.BuildConfig;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,9 +16,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.kremes.kremeswt.database.KremesDatabase.DB_NAME;
 import static com.kremes.kremeswt.utils.GeneralUtils.formatCurrentDate;
+
+import androidx.core.content.FileProvider;
 
 /**
  * Created by Bota on 10/8/2017.
@@ -44,11 +50,16 @@ public class BackupUtils {
     }
 
     public static void shareBackupFile(final Context context) {
-        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-        Uri databaseUri = Uri.parse(getDatabaseFilePath(context));
-        sharingIntent.setType("*/*");
-        sharingIntent.putExtra(Intent.EXTRA_STREAM, databaseUri);
-        context.startActivity(Intent.createChooser(sharingIntent, "Share using"));
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Kremes Backup " + new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "body text");
+        Uri uri = getDatabaseFileUri(context);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        context.startActivity(Intent.createChooser(emailIntent, "Izaberi Email providera"));
     }
 
     private static String getDatabaseFilePath(Context context){
@@ -56,5 +67,15 @@ public class BackupUtils {
         String currentDBPath = "/data/" + context.getPackageName() + "/databases/" + DB_NAME;
         File currentDB = new File(data, currentDBPath);
         return currentDB.getAbsolutePath();
+    }
+
+    private static Uri getDatabaseFileUri(Context context){
+        File data = Environment.getDataDirectory();
+        String currentDBPath = "/data/" + context.getPackageName() + "/databases/" + DB_NAME;
+        File currentDB = new File(data, currentDBPath);
+        return FileProvider.getUriForFile(
+                context,
+                "com.kremes.kremeswt" + ".provider",
+                currentDB);
     }
 }
